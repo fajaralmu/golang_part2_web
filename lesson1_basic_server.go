7712d6984a260bcdecb005a922984d197a630d7f
Fajar AM <somabangsa@gmail.com>
2020-07-22 21:43:32 +0700

    update

--- lesson1_basic_server.go
+++ lesson1_basic_server.go
@@ -4,9 +4,26 @@ import "net/http"
 
 func lesson1Run() {
 	println("__lesson1Run__")
-	multiPlexor := http.NewServeMux()
-	http.HandleFunc("/", basePage)
-	http.ListenAndServe(PORT, multiPlexor)
+	// http.HandleFunc("/basepage", basePage)
+	oneSampleTwo()
+}
+
+func oneSampleOne() {
+	http.HandleFunc("/basepage", basePage)
+}
+func oneSampleTwo() {
+	theUser := &user{name: "Fajar"}
+
+	http.ListenAndServe(PORT, theUser)
+}
+
+type user struct {
+	name string
+}
+
+//implemented method(ServeHTTP)
+func (u *user) ServeHTTP(w http.ResponseWriter, res *http.Request) {
+	w.Write([]byte("Name: " + u.name))
 }
 
 func basePage(w http.ResponseWriter, res *http.Request) {
