362a350bd516c98b2385f488854e7a29287f59e3
Fajar AM <somabangsa@gmail.com>
2020-07-23 11:41:06 +0700

    array loop in html template

--- src/main/templates.go
+++ src/main/templates.go
@@ -23,6 +23,13 @@ const htmlTemplate = `
 	id not 3
 	{{end}}
 	</p>
+	<hr/>
+	<p>Articles</p>
+	<ul>
+		{{range .Articles}}
+		<li>{{.}}</li> 
+		{{end}}
+	</ul>
 	</body>
 </html>
 `
@@ -33,16 +40,19 @@ func main() {
 }
 
 type pageObject struct {
-	ID      int
-	Title   string
-	Message string
+	ID       int
+	Title    string
+	Message  string
+	Articles []string
 }
 
 func oneFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/html")
 	html, err := template.New("templateName").Parse(htmlTemplate)
 	if err == nil {
-		_pageObject := pageObject{3, "Page One", "Hello Go"}
+		_pageObject := pageObject{3, "Page One", "Hello Go", []string{
+			"News #1", "News #2", "News #3",
+		}}
 		html.Execute(w, _pageObject)
 	}
 }
